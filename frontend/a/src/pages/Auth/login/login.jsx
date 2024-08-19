50765b5e06a15cc11e1bbbe4c6c7a3124f27ad16
rawr <110759203+UrWebDev@users.noreply.github.com>
2024-08-19 11:39:28 +0800

    Update login jsx with auth

--- frontend/a/src/pages/Auth/login/login.jsx
+++ frontend/a/src/pages/Auth/login/login.jsx
@@ -1,26 +1,57 @@
-import React from 'react'
+import React,{useState} from 'react'
+import './login.css'
+import { useNavigate } from 'react-router-dom'
 
 const login = () => {
+  const navigate = useNavigate()
+  const [formData, setFormData] = useState({
+    email: '',
+    password: '',
+  })
+
+  const changeHandler = (e)=>{
+    setFormData({...formData, [e.target.name]:e.target.value})
+}
+
+const handleSubmitButton = async (e) => {
+  e.preventDefault();
+  try {
+    const response = await fetch('http://localhost:3000/auth/login', {
+      method: "POST",
+      headers:{
+        'Content-Type': 'application/json'
+      },
+      body: JSON.stringify(formData)
+    });
+    if (!response.ok) {
+      throw new Error('Network response was not ok');
+    }
+    const result = await response.json();
+    console.log(result);
+    navigate('/inventory');
+  } catch (error) {
+    console.error("Error: ", error);
+  }
+};
+
+
   return (
     <>
-      <section className='container'>
-        <div>Signup/Register</div>
+      <section className='containerr'>
+        <div>Login</div>
         <div className="content">
-        <form action="POST">
-          <div className="input-box">
-            <label htmlFor="">Name</label>
-            <input type="text" name='username' placeholder='username'/>
-          </div>
+        <form>
           <div className="input-box">
             <label htmlFor="">Email</label>
-          <input type="email" name='email' placeholder='email'/>
+          <input type="email" name='email' placeholder='email' onChange={changeHandler} value={formData.email}/>
           </div>
           <div className="input-box">
             <label htmlFor="">Password</label>
-          <input type="text" name='password' placeholder='password'/>
+          <input type="password" name='password' placeholder='password' onChange={changeHandler} value={formData.password}/>
           </div>
           {/* or navlink? IDK :< */}
-          <button type='submit'>Login</button>
+          {/* {onClick={(e) => {handleSubmitButton(e)}} pwede sa button or sa form since submit type naman yung button basta nasa loob ng form} */}
+          <button type='submit' onClick={(e) => {handleSubmitButton(e)}}>Login</button>
         </form>
         </div>
       </section>
