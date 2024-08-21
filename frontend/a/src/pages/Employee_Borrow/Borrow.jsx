49662984396d94a822f2fdfe3f05f3cd4b636678
rawr <110759203+UrWebDev@users.noreply.github.com>
2024-08-21 09:32:26 +0800

    Rendered the data in user's side

--- frontend/a/src/pages/Employee_Borrow/Borrow.jsx
+++ frontend/a/src/pages/Employee_Borrow/Borrow.jsx
@@ -1,54 +1,34 @@
-import React from 'react'
-import './Borrow.css'
-const Borrow = () => {
-  return (
-    <div className="inventory-container">
-      <div className="inventory-heading">
-        <h3>Products</h3>
-      </div>
-      <table className="inventory-table">
-        <thead>
-          <tr>
-            <th className="title">Product_ID</th>
-            <th className="title">Name</th>
-            <th className="title">Brand</th>
-            <th className="title">Quantity</th>
-            <th className="title">Category</th>
-            <th className="title">Borrow</th>
-          </tr>
-        </thead>
-        <tbody>
-          <tr>
-            <td className="data" >000</td>
-            <td className="data">Laptop</td>
-            <td className="data">Asus</td>
-            <td className="data">13</td>
-            <td className="data">Peripherals</td>
-          </tr>
-          <tr>
-            <td className="data">000</td>
-            <td className="data">Laptop</td>
-            <td className="data">Asus</td>
-            <td className="data">13</td>
-            <td className="data">Peripherals</td>
-          </tr>
-          <tr>
-            <td className="data">000</td>
-            <td className="data">Laptop</td>
-            <td className="data">Asus</td>
-            <td className="data">13</td>
-            <td className="data">Peripherals</td>
-          </tr>
-         
-        </tbody>
-      </table>
-      <div className="inventory-pagination">
-        <button className="inventory-button">Previous</button>
-        <span className="inventory-page-info">Page 1 of 10</span>
-        <button className="inventory-button">Next</button>
-      </div>
-    </div>
-  )
-}
+import React, { useEffect, useState } from 'react';
+import axios from 'axios';
+import BorrowProductTablee from "../../components/TableData/borrowProductTable"
 
-export default Borrow
+const Borrow = () => {
+  const [loading, setLoading] = useState(true);
+  const [data, setData] = useState([]);
+
+  useEffect(() => {
+    const fetchData = async () => {
+      setLoading(true);
+      try {
+        const { data: response } = await axios.get('http://localhost:3000/all-borrow-products');
+        setData(response);
+      } catch (error) {
+        console.error(error.message);
+      }
+      setLoading(false);
+    };
+
+    fetchData();
+  }, []);
+
+  return (
+    <div>
+      {loading && <div>Loading...</div>}
+      {!loading && (
+        <BorrowProductTablee products = {data} />
+      )}
+    </div>
+  );
+};
+
+export default Borrow;
