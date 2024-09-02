be8ae04b6a7fada137a4688ce6ec69b7070a90b8
rawr <110759203+UrWebDev@users.noreply.github.com>
2024-09-02 14:20:32 +0800

    sdiebar css user fixed

--- frontend/a/src/components/Sidebar/Sidebar.jsx
+++ frontend/a/src/components/Sidebar/Sidebar.jsx
@@ -1,27 +1,71 @@
-
 import { NavLink } from 'react-router-dom';
 import './Sidebar.css';
-
+import { useState } from 'react';
 const Sidebar = () => {
-  return (
-    <div className="navbar">
-        <h1 className="logo">Launchpad</h1>
-      <div className="choices">
-        <NavLink to={'/'} className="pages">Dashboard</NavLink>
-        <NavLink to={'/inventory'} className="pages">Inventory</NavLink>
-        {/* <NavLink to={'/borrow'} className="pages">Borrow</NavLink> */}
+  const [reservationDropdownOpen, setReservationDropdownOpen] = useState(false);
 
-        <NavLink to={'/userCategory/hardwares'} className="pages">Hardwares</NavLink>
-                <NavLink to={'/userCategory/furnitures'} className="pages">Furnitures</NavLink>
-                <NavLink to={'/userCategory/appliances'} className="pages">Appliances</NavLink>    
-                <NavLink to={'/userCategory/lightings'} className="pages">Lightings</NavLink>    
-                <NavLink to={'/userCategory/others'} className="pages">Others</NavLink>
-      </div>
-    </div>
-  );
-}
+  const toggleReservationDropdown = () => {
+    setReservationDropdownOpen(!reservationDropdownOpen);
+
+  };
+
+    return (
+        <nav className="navbar">
+        <div className="logo">Launchpad</div>
+              <li className="choices">
+              <NavLink to={'/'} className="pages">Dashboard</NavLink>
+              </li>
+              <li 
+              className="choices" 
+              onClick={toggleReservationDropdown}
+              >
+              <span className="dropbtn">Inventory</span>
+              {reservationDropdownOpen && (
+                  <div className="dropdown-content">
+                  <NavLink to={'/inventory'} >All Products</NavLink>
+                  <NavLink to={'/userCategory/hardwares'} >Hardwares</NavLink>
+               <NavLink to={'/userCategory/furnitures'} >Furnitures</NavLink>
+              <NavLink to={'/userCategory/appliances'} >Appliances</NavLink>    
+              <NavLink to={'/userCategory/lightings'} >Lightings</NavLink>    
+               <NavLink to={'/userCategory/others'} >Others</NavLink>  
+                  </div>
+              )}
+              </li>
+      </nav>
+    );
+  };
+  
+
+
+
 
 export default Sidebar;
+
+
+
+
+
+
+// const Sidebar = () => {
+//   return (
+//     <div className="navbar">
+//         <h1 className="logo">Launchpad</h1>
+//       <div className="choices">
+//         <NavLink to={'/'} className="pages">Dashboard</NavLink>
+//         <NavLink to={'/inventory'} className="pages">Inventory</NavLink>
+//         {/* <NavLink to={'/borrow'} className="pages">Borrow</NavLink> */}
+
+//         <NavLink to={'/userCategory/hardwares'} className="pages">Hardwares</NavLink>
+//                 <NavLink to={'/userCategory/furnitures'} className="pages">Furnitures</NavLink>
+//                 <NavLink to={'/userCategory/appliances'} className="pages">Appliances</NavLink>    
+//                 <NavLink to={'/userCategory/lightings'} className="pages">Lightings</NavLink>    
+//                 <NavLink to={'/userCategory/others'} className="pages">Others</NavLink>
+//       </div>
+//     </div>
+//   );
+// }
+
+// export default Sidebar;
 
 
 
