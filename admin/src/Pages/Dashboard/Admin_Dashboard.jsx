33dccef4255ea476753cd61ff7a7e8fdcb49fc6d
rawr <110759203+UrWebDev@users.noreply.github.com>
2024-09-03 21:21:01 +0800

    Reapply "Update CSS"
    This reverts commit 403b346a027c2c87026c94a93a1f3819476d7d7d.

--- admin/src/Pages/Dashboard/Admin_Dashboard.jsx
+++ admin/src/Pages/Dashboard/Admin_Dashboard.jsx
@@ -1,13 +1,13 @@
 import React from 'react';
-import AdminNotifications from '../../components/AdminNotifications/AdminNotifications';
+// import AdminNotifications from '../../components/AdminNotifications/AdminNotifications';
 import AdminDashboard from '../../components/AdminDashboard/AdminDashboard';
 import './Dashboard.css'
-0
+
 const Admin_Dashboard = () => {
   return (
     <div className='dashCont'>
       <AdminDashboard />
-      <AdminNotifications />
+      {/* <AdminNotifications /> */}
     </div>
   );
 };
