38cd3a435dfaebec73b93723b7ec9c3b077f9f63
charlysss-l <charlizemikaelanadela@gmail.com>
2024-09-05 13:32:37 +0800

    Update server.js

--- backend/server.js
+++ backend/server.js
@@ -19,7 +19,7 @@ import signupRoute from './src/routes/signup.js';
 import loginRoute from './src/routes/login.js'
 
 const app = express();
-const port = 3000;
+const port = process.env.PORT || 3000;
 app.use(express.json());
 
 //sign roure
